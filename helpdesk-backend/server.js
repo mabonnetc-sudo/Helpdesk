d2e21eb9ea16b9ccca70330ce499df22f2165c7f
mabonnetc-sudo <mabonnetc@sanmateo.edu.co>
2025-12-09 20:50:01 -0500

    Update server.js

--- helpdesk-backend/server.js
+++ helpdesk-backend/server.js
@@ -16,7 +16,7 @@ app.use("/cases", caseRoutes);
 app.use("/reports", reportRoutes);
 
 app.get("/", (req, res) => {
-    res.json({ message: "Helpdesk API funcionando correctamente sapos perros" });
+    res.json({ message: "Helpdesk API funcionando correctamente sapos perros esto lo hizo Nicolai PTE" });
 });
 
 const PORT = process.env.PORT || 3000;
